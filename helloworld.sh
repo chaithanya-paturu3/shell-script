8ec4285f72631cd3437f39c26a4339d0e75b7151
chaithanya-paturu3 <paturuchaithanya7@gmail.com>
2025-01-20 17:40:30 -0500

    adding variables

--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "chaithu : Hi santhi"
-echo "santhi  :  hi"
-echo "chaithu : how have you been"
-echo "santhi : well going good 
+echo "person1 : Hi santhi"                   #person1=chaithu
+echo "person2  :  hi"                        #person2=santhi
+echo "person1 : how have you been"
+echo "person2 : well going good 
